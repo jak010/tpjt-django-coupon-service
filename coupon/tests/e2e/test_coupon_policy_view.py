bfab8cf01ce8ac4e06d103966c78999144c0c937
jak010 <bluetoon@naver.com>
2025-06-01 12:00:34 +0900

    feat: 쿠폰 정책 API 테스트 코드 개선 (main)
    - API 요청 URL을 상수화하여 코드 가독성 및 유지보수성 향상
    - 테스트 코드 가독성 향상을 위해 Arrange, Act, Assert 구분 명확화
    - `test_create_percent_amount__coupon_policy_view` 함수명 수정

--- coupon/tests/e2e/test_coupon_policy_view.py
+++ coupon/tests/e2e/test_coupon_policy_view.py
@@ -1,9 +1,7 @@
 import json
 
-from rest_framework.test import APIRequestFactory, APIClient
-
-from coupon.views.coupon_policy_view import CouponPolicyView
 import pytest
+from rest_framework.test import APIRequestFactory, APIClient
 
 
 @pytest.mark.django_db
@@ -13,52 +11,54 @@ class TestCreateCouponPolicyView:
         self.factory = APIRequestFactory()
         self.client = APIClient()
 
+        self.request_url = '/api/v1/coupon/policy'
+
     def test_create_fixed_amount_coupon_policy_view(self):
         """ 쿠폰 정책, 정액 할인 생성 테스트 """
 
+        # Arrange
+        request_data = {
+            "name": "여름 프로모션 10% 할인",
+            "discount_type": "FIXED_AMOUNT",
+            "discount_value": 10,
+            "minimum_order_amount": 30000,
+            "maximum_order_amount": 150000,
+            "total_quantity": 1000,
+            "start_time": "2025-07-01",
+            "end_time": "2025-07-31"
+        }
+
         # Act
         response = self.client.post(
-            '/api/v1/coupon/policy',
+            self.request_url,
             content_type="application/json",
-            data=json.dumps(
-                {
-                    "name": "여름 프로모션 10% 할인",
-                    "discount_type": "FIXED_AMOUNT",
-                    "discount_value": 10,
-                    "minimum_order_amount": 30000,
-                    "maximum_order_amount": 150000,
-                    "total_quantity": 1000,
-                    "start_time": "2025-07-01",
-                    "end_time": "2025-07-31"
-                }
-            )
+            data=json.dumps(request_data)
         )
         # Arrange
 
         # Assert
         assert response.status_code == 200
 
-    def test_create_percent_amount__coupon_policy_view(self):
+    def test_create_percent_amount_coupon_policy_view(self):
         """ 쿠폰 정책, 정률 할인 생성 테스트 """
+        # Arrange
+        request_data = {
+            "name": "여름 프로모션 10% 할인",
+            "discount_type": "PERCENTAGE",
+            "discount_value": 10,
+            "minimum_order_amount": 30000,
+            "maximum_order_amount": 150000,
+            "total_quantity": 1000,
+            "start_time": "2025-07-01T00:00:00Z",
+            "end_time": "2025-07-31T23:59:59Z"
+        }
 
         # Act
         request = self.client.post(
-            '/api/v1/coupon-policy',
+            self.request_url,
             content_type="application/json",
-            data=json.dumps(
-                {
-                    "name": "여름 프로모션 10% 할인",
-                    "discount_type": "PERCENTAGE",
-                    "discount_value": 10,
-                    "minimum_order_amount": 30000,
-                    "maximum_order_amount": 150000,
-                    "total_quantity": 1000,
-                    "start_time": "2025-07-01T00:00:00Z",
-                    "end_time": "2025-07-31T23:59:59Z"
-                }
-            )
+            data=json.dumps(request_data)
         )
-        # Arrange
 
         # Assert
         assert request.status_code == 200
